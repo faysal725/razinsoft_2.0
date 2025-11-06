50a9c534263a16b3ad7f88153c9996ac241fe27d
faysal725 <faysal.dev@gmail.com>
2025-11-06 11:16:11 +0600

    assets folder modified

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -1,11 +1,9 @@
+import Navbar from "@/components/Navbar";
+
 export default function PublicLayout({ children }) {
   return (
     <div className="relative">
-        <nav className="bg-white border-b border-b-gray-50 py-4 lg:py-6">
-            <div className="bg-amber-700 container">
-                asdfasdfsdf
-            </div>
-        </nav>
+        <Navbar />
 
         <main className="min-h-[90vh]">
             {children}
--- app/(public)/page.js
+++ app/(public)/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="text-5xl">
-      asdfasdfsdf
+      lorem5000
     </div>
   );
 }

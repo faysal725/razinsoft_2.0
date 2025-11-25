18756471a547b11a24927169de377bf2cacc15eb
faysal725 <faysal.dev@gmail.com>
2025-11-25 10:41:40 +0600

    authslice modified

--- store/slices/authSlice.js
+++ store/slices/authSlice.js
@@ -2,13 +2,23 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
     name: "auth",
-    initialState: { authenticated: false, user: null, token: null, token_no: null, value: 0 },
+    initialState: {
+        authenticated: false,
+        user: null,
+        token: null,
+        token_no: null,
+        value: 0,
+        loading: false,
+        error: null,
+    },
     reducers: {
-        increment: (state) => {
-            state.value += 1;
-        },
-        decrement: (state) => {
-            state.value -= 1;
+        
+        
+        logout(state) {
+            state.user = false;
+            state.token = null;
+            state.authenticated = null;
+            state.token_no = null
         },
     },
 });
